bb326d2e4f0b6bcbd60f0fc8f4a0dc4d2f21c0b0
ibrahimuzunoglu <86494575+ibrahimuzunoglu@users.noreply.github.com>
2024-10-31 20:00:45 +0300

    app.py

--- Futbol_Takimlar/app.py
+++ Futbol_Takimlar/app.py
@@ -1,46 +1,67 @@
 from flask import Flask, request, send_file, jsonify
 from PIL import Image, ImageDraw, ImageFont
 import io
+import urllib.parse
+import pymongo
+
 
 app = Flask(__name__)
+
+username = "mongadmin"
+password = "tu6@fKhW@Sepr8oFhB"
+encoded_username = urllib.parse.quote_plus(username)
+encoded_password = urllib.parse.quote_plus(password)
+client = pymongo.MongoClient(f"mongodb://{encoded_username}:{encoded_password}@78.111.98.157:27017/admin")
+db = client["datablastest"]
+collection = db["soccerteams"]
 
 @app.route('/generate_image', methods=['POST'])
 def generate_image():
     data = request.json
     print(data)
-    
+
     stadium_name = data.get('stadium_name')
     match_date = data.get('match_date')
     match_time = data.get('match_time')
-    team1_name = data.get('team1')
-    team2_name = data.get('team2')
+
+    team_1_provider = data.get('team_1_provider')
+    team_2_provider = data.get('team_2_provider')
+    team1_id = data.get('team1')
+    team2_id = data.get('team2')
     league = data.get('league')
 
-    if not all([stadium_name, match_date, match_time, team1_name, team2_name, league]):
+    if not all([stadium_name, match_date, match_time, team1_id, team2_id, league]):
         return jsonify({'error': 'Missing data'}), 400
 
     if league == 1:
-        logo_dir = "./logo"
-        small = 1
+        logo_dir = r"/home/ibrahimuzn/image_api/Futbol_Takimlar/logo"
+        small = 1.50
     else:
         return jsonify({'error': 'Invalid league'}), 400
+    
+    team1 = collection.find_one({"providers."+{team_1_provider}+".id": team1_id})
+    team2 = collection.find_one({"providers."+{team_2_provider}+".id": team2_id})
 
-    bg = Image.open("bg/bg.png")
+    if not team1 or not team2:
+        return jsonify({'error': 'One or more team logos not found in the database'}), 404
+
+    bg = Image.open("bg/bg1.png")
     bg_w, bg_h = bg.size
 
     try:
-        team1_logo = Image.open(f"{logo_dir}/{team1_name}.png")
+        #team_1_name - sorgu = pathch
+        team1_logo = Image.open(f"{logo_dir}/{team1['logo_url']}")
         img_w1, img_h1 = team1_logo.size
         team1_logo = team1_logo.resize((int(img_w1 * small), int(img_h1 * small)))
 
-        team2_logo = Image.open(f"{logo_dir}/{team2_name}.png")
+        team2_logo = Image.open(f"{logo_dir}/{team2['logo_url']}")
         img_w2, img_h2 = team2_logo.size
         team2_logo = team2_logo.resize((int(img_w2 * small), int(img_h2 * small)))
     except FileNotFoundError:
         return jsonify({'error': 'One or more team logos not found'}), 404
 
-    offset1 = ((bg_w - int(img_w1 * small)) // 4, (bg_h - int(img_h1 * small)) // 2)
-    offset2 = ((bg_w - int(img_w2 * small)) * 3 // 4, (bg_h - int(img_h2 * small)) // 2)
+    offset1 = ((bg_w - int(img_w1 * small)) // 4, (bg_h - int(img_h1 * small)) // 2 - 20)
+    offset2 = ((bg_w - int(img_w2 * small)) * 3 // 4, (bg_h - int(img_h2 * small)) // 2 - 20)
     bg.paste(team1_logo, offset1, mask=team1_logo)
     bg.paste(team2_logo, offset2, mask=team2_logo)
 
@@ -53,21 +74,21 @@ def generate_image():
     text_w = bbox[2] - bbox[0]
     text_h = bbox[3] - bbox[1]
     text_x = (bg_w // 2) + 50
-    text_y = (bg_h - (text_h // 2)) - 105
+    text_y = (bg_h - (text_h // 2)) - 185
     draw.text((text_x, text_y), stadium_name, font=font, fill="white")
 
     bbox_n = draw.textbbox((0, 0), match_date, font=font)
     text_w = bbox_n[2] - bbox_n[0]
     text_h = bbox_n[3] - bbox_n[1]
     text_x = ((bg_w // 2) - text_w) - 50
-    text_y = (bg_h - (text_h // 2)) - 117
+    text_y = (bg_h - (text_h // 2)) - 197
     draw.text((text_x, text_y), match_date, font=font, fill="white")
 
     bbox_n_n = draw.textbbox((0, 0), match_time, font=font)
     text_w = bbox_n_n[2] - bbox_n_n[0]
     text_h = bbox_n_n[3] - bbox_n_n[1]
     text_x = ((bg_w // 2) - text_w) - 50
-    text_y = (bg_h - (text_h // 2)) - 95
+    text_y = (bg_h - (text_h // 2)) - 175
     draw.text((text_x, text_y), match_time, font=font, fill="white")
 
     img_io = io.BytesIO()
